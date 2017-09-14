fcc1f1883428e6d952b232adc1e091ed0f4ad9a2
ChibiFR <23081995b@gmail.com>
2017-09-14 06:53:36 +0200

    Add layers index

---
+++ src/layers/index.ts
@@ -0,0 +1,7 @@
+export * from './AudioLayer';
+export * from './Circle';
+export * from './ImageLayer';
+export * from './Layer';
+export * from './Rectangle';
+export * from './Shape';
+export * from './TextLayer';
