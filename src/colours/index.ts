a89c27f207c20dd19aa215f4cb49de5715e040c5
ChibiFR <23081995b@gmail.com>
2017-09-14 06:51:39 +0200

    Add colours index

---
+++ src/colours/index.ts
@@ -0,0 +1,6 @@
+export * from './Colour';
+export * from './Gradiant';
+export * from './LinearGradiant';
+export * from './Pattern';
+export * from './RadialGradiant';
+export * from './RGBColour';
